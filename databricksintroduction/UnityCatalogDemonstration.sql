51fc38ac77bb97500ca64b95471612e2834990b2
bibhutigithub <bibhutigithub>
2024-04-20 18:44:18 +0000

    unity catalog operations

--- databricksintroduction/UnityCatalogDemonstration.sql
+++ databricksintroduction/UnityCatalogDemonstration.sql
@@ -1,2 +1,18 @@
 -- Databricks notebook source
+CREATE CATALOG IF NOT EXISTS dev;
 
+-- COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS dev.learning_db;
+
+-- COMMAND ----------
+
+DROP DATABASE IF EXISTS dev.default;
+
+-- COMMAND ----------
+
+GRANT USE CATALOG ON CATALOG dev TO `dataeng-grp`
+
+-- COMMAND ----------
+
+GRANT ALL PRIVILEGES ON DATABASE dev.learning_db TO `dataeng-grp`
