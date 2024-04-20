bdd54536c27881205df205800e0db1ab7f6c6e4e
bibhutigithub <bibhutigithub>
2024-04-20 19:35:10 +0000

    Unity Catalog commands

--- databricksintroduction/UnityCatalogDemonstration.sql
+++ databricksintroduction/UnityCatalogDemonstration.sql
@@ -16,3 +16,19 @@ GRANT USE CATALOG ON CATALOG dev TO `dataeng-grp`
 -- COMMAND ----------
 
 GRANT ALL PRIVILEGES ON DATABASE dev.learning_db TO `dataeng-grp`
+
+-- COMMAND ----------
+
+DESCRIBE EXTERNAL LOCATION `databricks-operations-datalake-ext-location`;
+
+-- COMMAND ----------
+
+GRANT ALL PRIVILEGES ON EXTERNAL LOCATION `databricks-operations-datalake-ext-location` TO `dataeng-grp`;
+
+-- COMMAND ----------
+
+GRANT ALL PRIVILEGES ON EXTERNAL LOCATION `learningdatalakestorage_ext_location` TO `dataeng-grp`;
+
+-- COMMAND ----------
+
+-- MAGIC %fs ls abfss://datastore@learningdatalakestorage.dfs.core.windows.net/csvdata/
